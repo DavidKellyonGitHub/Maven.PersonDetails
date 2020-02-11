3d5ea10cda4c80e9edc324e6cc594ef307e6496d
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-11 14:22:08 -0500

    for loop completed

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -14,31 +14,21 @@ public class PersonHandler {
         String result = "";
         // create a `counter`
         int currentPersonIndex = 0;
-        while (currentPersonIndex < personArray.length){
+        while (currentPersonIndex < personArray.length) {
             String nameOfPerson = personArray[currentPersonIndex].toString();
             result += nameOfPerson;
             currentPersonIndex++;
-
         }
-        // while `counter` is less than length of array
-            // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
         return result;
     }
 
-
-
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
 
+        for (int currentPersonIndex = 0; currentPersonIndex < personArray.length; currentPersonIndex++){
+            String nameOfPerson = personArray[currentPersonIndex].toString();
+            result += nameOfPerson;
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
